36499bad9db49e6a859d0132b13e3b2fa38699bc
Throghar <pawel.mirgos@gmail.com>
2022-02-16 23:14:08 +0100

    Added more tests along with implementation with basic selection. Added distinct keyword tests + implementation
    Signed-off-by: Throghar <pawel.mirgos@gmail.com>

--- sql_query.h
+++ sql_query.h
@@ -17,7 +17,7 @@ class SQLQuery {
         return *this;
     }
     SQLQuery& Database(const std::string& database_name) {
-        query_ += "DATABASE " + database_name;
+        query_ += "DATABASE " + database_name + " ";
         return *this;
     }
     SQLQuery& Drop() {
@@ -35,11 +35,26 @@ class SQLQuery {
     }
 
     SQLQuery& SELECT() {
+        if(!select_started_) {
+            query_ += "SELECT";
+        }
+        query_ += std::string(" ");
         select_started_ = false;
         return *this;
     }
 
-    const std::string& ToString() const {
+    SQLQuery& FROM(const std::string& from) {
+        query_ += "FROM " + from + std::string(" ");
+        return *this;
+    }
+
+    SQLQuery& DISTINCT(const std::string& distinct) {
+        query_ += "DISTINCT " + distinct + " ";
+        return *this;
+    }
+
+    const std::string& ToString() {
+        query_.erase(query_.length()-1);
         return query_;
     }
     private:
--- sql_query_test.cc
+++ sql_query_test.cc
@@ -4,12 +4,12 @@
 
 TEST(SqlQueryTest,ShowDatabase) {
     auto cmd = SQLQuery().Show().Databases().ToString();
-    EXPECT_STREQ(cmd.c_str(),"SHOW DATABASES ");
+    EXPECT_STREQ(cmd.c_str(),"SHOW DATABASES");
 }
 
 TEST(SqlQueryTest,DropDatabase) {
     auto cmd = SQLQuery().Drop().Databases().ToString();
-    EXPECT_STREQ(cmd.c_str(),"DROP DATABASES ");
+    EXPECT_STREQ(cmd.c_str(),"DROP DATABASES");
 }
 
 TEST(SqlQueryTest,CreateDatabase) {
@@ -17,12 +17,47 @@ TEST(SqlQueryTest,CreateDatabase) {
     EXPECT_STREQ(cmd.c_str(),"CREATE DATABASE test_db");
 }
 
-TEST(SqlQueryTest,SelectAll) {
+TEST(SqlQueryTestSelectingColumns,SelectAll) {
     auto cmd = SQLQuery().SELECT("*").ToString();
     EXPECT_STREQ(cmd.c_str(),"SELECT *");
 }
 
-TEST(SqlQueryTest,SelectTwoRecord) {
+TEST(SqlQueryTestSelectingColumns,SelectAllFrom) {
+    auto cmd = SQLQuery().SELECT("*").FROM("test_db").ToString();
+    EXPECT_STREQ(cmd.c_str(),"SELECT * FROM test_db");
+}
+
+TEST(SqlQueryTestSelectingColumns,SelectTwoRecords) {
     auto cmd = SQLQuery().SELECT("name","surname").ToString();
     EXPECT_STREQ(cmd.c_str(),"SELECT name, surname");
+}
+
+TEST(SqlQueryTestSelectingColumns,SelectMultipleRecords) {
+    auto cmd = SQLQuery().SELECT("name","surname","age","sex").ToString();
+    EXPECT_STREQ(cmd.c_str(),"SELECT name, surname, age, sex");
+}
+
+TEST(SqlQueryTestSelectingColumns,SelectRecordFromDatabase) {
+    auto cmd = SQLQuery().SELECT("name").FROM("test_db").ToString();
+    EXPECT_STREQ(cmd.c_str(),"SELECT name FROM test_db");
+}
+
+TEST(SqlQueryTestSelectingColumns,SelectTwoRecordsFromDatabase) {
+    auto cmd = SQLQuery().SELECT("name","surname").FROM("test_db").ToString();
+    EXPECT_STREQ(cmd.c_str(),"SELECT name, surname FROM test_db");
+}
+
+TEST(SqlQueryTestSelectingColumns,SelectMultipleRecordsFromDatabase) {
+    auto cmd = SQLQuery().SELECT("name","surname","age","sex").FROM("test_db").ToString();
+    EXPECT_STREQ(cmd.c_str(),"SELECT name, surname, age, sex FROM test_db");
+}
+
+TEST(SqlQueryTestSelectingColumns,SelectDistinct) {
+    auto cmd = SQLQuery().SELECT().DISTINCT("name").ToString();
+    EXPECT_STREQ(cmd.c_str(),"SELECT DISTINCT name");
+}
+
+TEST(SqlQueryTestSelectingColumns,SelectDistinctFromDatabase) {
+    auto cmd = SQLQuery().SELECT().DISTINCT("name").FROM("test_db").ToString();
+    EXPECT_STREQ(cmd.c_str(),"SELECT DISTINCT name FROM test_db");
 }
